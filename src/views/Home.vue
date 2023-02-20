d33b0e8592ccd6497c63e3fd0ed50273eba7d240
Alex <lex.courtney18@gmail.com>
2023-02-20 10:06:18 -0600

    create postlist and singlepost components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,28 @@
 <template>
   <div class="home">
   <h1>Home</h1>
-  <input type="text" v-model="search">
-  <p>search term - {{ search }} </p>
-  <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-  <button @click="handleClick">stop watching</button>
+  <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup() {
 
-    const search = ref('')
-
-    const names = ref(['max','lewis','george','daniel','lando','pierre','carlos'])
-
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function ran', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ', id: 1 },
+      { title: 'top 5 css tips', body: 'lorem ipsum', id: 2 },
+    ])
     
 
-    return { names, search, matchingNames, handleClick  }
+    return { posts }
   },
 }
 </script>
