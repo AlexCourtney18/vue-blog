2f1a804e06566a0ff7ff824d31143fabf7c0b7e7
Alex <lex.courtney18@gmail.com>
2023-02-20 09:33:32 -0600

    sample excercise to see functionality difference between ref and reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="home">
-  home
-  <p>My name is {{ name }} and my age is {{ age }}</p>
-  <button @click="handleClick">Click Here</button>
+  <h1>Home</h1>
+  <h2>Refs</h2>
+  <p>{{ alexOne.name }} - {{ alexOne.age }}</p>
+  <button @click="updateAlexOne">Update Alex One</button>
+  <h2>Reactive</h2>
+  <p>{{ alexTwo.name }} - {{ alexTwo.age }} - {{ nameTwo }}</p>
+  <button @click="updateAlexTwo">Update Alex Two</button>
   </div>
 </template>
 
 <script>
+import { ref, reactive } from 'vue'
 
 
 
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
+    
+    const alexOne = ref({ name: 'Lewis', age: 44 })
+    const alexTwo = reactive({ name: 'George', age: 63 })
+    
+    const nameOne = ref('mario')
+    const nameTwo = reactive('luigi')
+    
 
-    let name = 'mario'
-    let age = 30
-
-    const handleClick = () => {
-      console.log('you clicked me')
+    const updateAlexOne = () => {
+      alexOne.value.age = 35
     }
 
-    return { name, age, handleClick}
+    const updateAlexTwo = () => {
+      alexTwo.age = 25
+      nameTwo = 'Mario'
+    }
+
+    return { updateAlexOne, alexOne, alexTwo, updateAlexTwo, nameTwo }
   },
 }
 </script>
