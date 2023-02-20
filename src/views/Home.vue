27f08f8dd1e6bfbb3430512cc6116524ae9cf83f
Alex <lex.courtney18@gmail.com>
2023-02-20 09:44:14 -0600

    add a computed property inside setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,31 @@
 <template>
   <div class="home">
   <h1>Home</h1>
-  <h2>Refs</h2>
-  <p>{{ alexOne.name }} - {{ alexOne.age }}</p>
-  <button @click="updateAlexOne">Update Alex One</button>
-  <h2>Reactive</h2>
-  <p>{{ alexTwo.name }} - {{ alexTwo.age }} - {{ nameTwo }}</p>
-  <button @click="updateAlexTwo">Update Alex Two</button>
+  <input type="text" v-model="search">
+  <p>search term - {{ search }} </p>
+  <div v-for="name in matchingNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { computed, ref } from 'vue'
 
 
 
 export default {
   name: 'Home',
   setup() {
-    
-    const alexOne = ref({ name: 'Lewis', age: 44 })
-    const alexTwo = reactive({ name: 'George', age: 63 })
-    
-    const nameOne = ref('mario')
-    const nameTwo = reactive('luigi')
+
+    const search = ref('')
+
+    const names = ref(['max','lewis','george','daniel','lando','pierre','carlos'])
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
     
 
-    const updateAlexOne = () => {
-      alexOne.value.age = 35
-    }
-
-    const updateAlexTwo = () => {
-      alexTwo.age = 25
-      nameTwo = 'Mario'
-    }
-
-    return { updateAlexOne, alexOne, alexTwo, updateAlexTwo, nameTwo }
+    return { names, search, matchingNames }
   },
 }
 </script>
